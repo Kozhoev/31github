740ca72a38516a2828f1f71696e3d90e08df7b57
Kozhoev <152251937+Kozhoev@users.noreply.github.com>
2023-12-03 19:11:53 +0600

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,11 @@
+public class Variables{
+    public static void main(String[] arg) {
+    String s1 = new String("where are you from?");
+    String s2 = "where?";
+    String s3 = s1 + s2;
+
+    System.out.println(s1);
+    System.out.println(s2);
+    System.out.println(s3);
+    }
+}
