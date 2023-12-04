0e613ae7a64f5f9457de087f62e18ed950972a73
Kozhoev <kozhoevmamazait@gmail.com>
2023-12-04 14:08:59 +0600

    ifelse.java was added

--- Variables.java
+++ Variables.java
@@ -1,11 +1,20 @@
-public class Variables{
+public class Variables {
     public static void main(String[] arg) {
-    String s1 = new String("where are you from?");
-    String s2 = "where?";
-    String s3 = s1 + s2;
+        String s1 = new String("where are you from?");
+        String s2 = "where?";
+        String s3 = s1 + s2;
 
-    System.out.println(s1);
-    System.out.println(s2);
-    System.out.println(s3);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+    }
+
+    static class Mymethod {
+        public static void main(String[] args) {
+            byte zero = 0;
+            String output = "W" + zero + "w";
+            System.out.println(output);
+        }
     }
 }
+
