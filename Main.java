0bcf2ad7d45a237676560ce10dcb692a16615b53
Kozhoev <kozhoevmamazait@gmail.com>
2023-12-04 16:01:26 +0600

    new helloworld method

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        System.out.println("Hello, World!");
+//
+//    }
+//
+//}
 public class Main {
-
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
-
+        byte zero = 0;
+        short three = 3;
+        int b = 1;
+        long c = 10;
+        char d = ' ';
+        float e = 2.0f;
+        boolean f = true;
+        String output = "H" + three + b + c + d + "w" + zero + "r" + b + "d" + d + e + d + f;
+        System.out.println(output);
     }
-
 }
