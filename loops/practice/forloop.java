c3738546e3fa40a9a952e6c782d19afb83d3bdd7
Kozhoev <kozhoevmamazait@gmail.com>
2023-12-05 12:06:30 +0600

    practice 2

--- loops/practice/forloop.java
+++ loops/practice/forloop.java
@@ -6,9 +6,13 @@ public class forloop {
             System.out.println("I love you");
         }
         int i = 0;
-        do {System.out.println("I hate you");
-            i++;
-        } while (i<7);
-
+//        do {System.out.println("I hate you");
+//            i++;
+//        } while (i<7);
+        int[] arr = {1, 9, 9, 5};
+        for (; i < arr.length; i++) {
+            int el = arr[i];
+            System.out.println(el);
+        }
 }
 }
