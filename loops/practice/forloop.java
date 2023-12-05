9cd6501566cb250b3eb41aaf0b0b34ddf2bbd988
Kozhoev <kozhoevmamazait@gmail.com>
2023-12-05 12:04:36 +0600

    forloop practice

---
+++ loops/practice/forloop.java
@@ -0,0 +1,14 @@
+package loops.practice;
+
+public class forloop {
+    public static void main(String[] args) {
+        for (int i = 0; i<5; i++) {
+            System.out.println("I love you");
+        }
+        int i = 0;
+        do {System.out.println("I hate you");
+            i++;
+        } while (i<7);
+
+}
+}
