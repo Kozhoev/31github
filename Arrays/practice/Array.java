b5de75f939f929496fbda1b0838bf6199edbe6fc
Kozhoev <kozhoevmamazait@gmail.com>
2023-12-06 10:40:25 +0600

    practice ...

---
+++ Arrays/practice/Array.java
@@ -0,0 +1,13 @@
+package Arrays.practice;
+
+public class Array {
+    public static void main(String[] args) {
+        int [] arr ;
+        arr = new int[5];
+        arr[0] = 1;
+        int i = 0;
+        for (; i <arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
