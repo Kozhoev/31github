677bfab4feb53489bbc22de41864d5fa8fe923c3
Kozhoev <kozhoevmamazait@gmail.com>
2023-12-07 11:15:34 +0600

    sum of two numbers

--- Methods/practice/Practice.java
+++ Methods/practice/Practice.java
@@ -3,11 +3,18 @@ package Methods.practice;
 public class Practice {
     public static void main(String[] args) {
         practice(args);
+        Practice a = new Practice();
+        a.bar(2,3);
     }
     public static void foo() {
         System.out.println("print this");
     }
     static void practice(String[] args) {
         foo();
+    }
+
+    public void bar(int num1, int num2){
+        int result = num1+num2;
+        System.out.println(result);
     }
 }
