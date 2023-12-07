5815f20fde5e75a7282e3ec3522d00e77a16fe9a
Kozhoev <kozhoevmamazait@gmail.com>
2023-12-07 11:48:58 +0600

    problem was solved

--- Methods/problems/Problems.java
+++ Methods/problems/Problems.java
@@ -1,9 +1,27 @@
 package Methods.problems;
 
-import Methods.practice.Practice;
+class Student{
+    private String firstName;
+    private String lastName;
+    public Student(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    public void printFullName(){
+        System.out.println(firstName+" "+lastName);
+    }
+}
 
 public class Problems {
     public static void main(String[] args) {
-        Practice.main(args);
+        Student[] students = new  Student[]{
+                new Student("Morgan","Freeman"),
+                new Student("Brad", "Pitt"),
+                new Student("Kevin","Spacey"),
+        };
+        for (Student s: students){
+            s.printFullName();
+        }
     }
+
 }
