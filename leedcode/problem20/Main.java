597a6ceb1d8cd794af6f1bca2f28f35ebc50f004
Kozhoev <kozhoevmamazait@gmail.com>
2023-12-14 10:20:43 +0600

    solution to problem 27

--- leedcode/problem20/Main.java
+++ leedcode/problem20/Main.java
@@ -1,19 +1,26 @@
 package leedcode.problem20;
-
+import java.util.Arrays;
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k = 0;
-
-        for (int i = 0; i<nums.length; i++){
-            if (nums[i] != val){
-                nums[k] = nums[i];
-                k++;
+        int count = 0, edge = nums.length - 1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == val) {
+                while (edge > 0) {
+                    if (nums[edge] != val) {
+                        nums[i] = nums[edge];
+                        edge--;
+                        break;
+                    }
+                    edge--;
+                }
+                count++;
             }
-        System.out.println(nums);
         }
-        return k;
+        System.out.println(Arrays.toString(nums));
+        return nums.length - count;
     }
 }
+
 public class Main {
     public static void main(String[] args) {
         // Example array
@@ -23,6 +30,13 @@ public class Main {
         // Create an instance of the Solution class
         Solution solution = new Solution();
 
+        // Call the removeElement method
+        int result = solution.removeElement(nums, val);
 
+        // Display the modified array using Arrays.toString
+        System.out.println("Modified Array: " + Arrays.toString(nums));
+
+        System.out.println("Number of elements not equal to " + val + ": " + result);
     }
 }
+
