4203597746493985a38404a67f6897a6a05ca6f0
Kozhoev <kozhoevmamazait@gmail.com>
2023-12-16 11:55:15 +0600

    new short solution

--- leedcode/problem126/Solution.java
+++ leedcode/problem126/Solution.java
@@ -17,3 +17,12 @@ class Solution {
         return -1;
     }
 }
+class Solution {
+    public int singleNumber(int[] nums) {
+        int result = 0;
+        for (int num : nums) {
+            result ^= num;
+        }
+        return result;
+    }
+}
