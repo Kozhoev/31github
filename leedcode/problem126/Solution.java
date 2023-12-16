f93cc58478103639d2151a614cf2b555662142d3
Kozhoev <kozhoevmamazait@gmail.com>
2023-12-16 11:49:24 +0600

    solution to problem 126

---
+++ leedcode/problem126/Solution.java
@@ -0,0 +1,19 @@
+package leedcode.problem126;
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        for (int i=0; i<nums.length;i++){
+            boolean s = false;
+            for (int k = 0; k<nums.length; k++){
+                if (i!=k && nums[i]==nums[k]){
+                    s = true;
+                    break;
+                }
+            }
+            if (!s){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
