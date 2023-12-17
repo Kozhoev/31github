1db3e85980080c04ef008b8a96710f9248c95879
Kozhoev <kozhoevmamazait@gmail.com>
2023-12-17 16:37:23 +0600

    Solution to problem 13

--- leedcode/problem126/Solution.java
+++ leedcode/problem126/Solution.java
@@ -17,12 +17,12 @@ class Solution {
         return -1;
     }
 }
-class Solution {
-    public int singleNumber(int[] nums) {
-        int result = 0;
-        for (int num : nums) {
-            result ^= num;
-        }
-        return result;
-    }
-}
+//class Solution {
+//    public int singleNumber(int[] nums) {
+//        int result = 0;
+//        for (int num : nums) {
+//            result ^= num;
+//        }
+//        return result;
+//    }
+//}
