81616cedf07637e6c54c46d4baa27ec3bd3d596b
Kozhoev <kozhoevmamazait@gmail.com>
2023-12-18 21:03:01 +0600

    solution to problem 242

---
+++ leedcode/problem242/Solution.java
@@ -0,0 +1,16 @@
+package leedcode.problem242;
+import java.util.Arrays;
+
+public class Solution {
+    public static boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+        return Arrays.equals(sChars, tChars);
+    }
+
+
+}
