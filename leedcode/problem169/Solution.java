ec1c2f6da4e9d9248a9c2446133870c56acc7309
Kozhoev <kozhoevmamazait@gmail.com>
2023-12-22 14:49:21 +0600

    solution to 169

--- leedcode/problem169/Solution.java
+++ leedcode/problem169/Solution.java
@@ -1,9 +1,34 @@
 package leedcode.problem169;
-import java.util.Arrays;
+//import java.util.Arrays;
+//class Solution {
+//    public int majorityElement(int[] nums) {
+//        Arrays.sort(nums);
+//        int n = nums.length;
+//        return nums[n/2];
+//    }
+//}
+
 class Solution {
     public int majorityElement(int[] nums) {
-        Arrays.sort(nums);
-        int n = nums.length;
-        return nums[n/2];
+        int candidate = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == candidate) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    candidate = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        count = 0;
+        for (int num : nums) {
+            if (num == candidate) {
+                count++;
+            }
+        }
+        return candidate;
     }
 }
