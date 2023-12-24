6986f1a4b413657a77edf5d43ad9c4e063049786
Kozhoev <kozhoevmamazait@gmail.com>
2023-12-24 17:45:17 +0600

    run

--- leedcode/problem371/Solution.java
+++ leedcode/problem371/Solution.java
@@ -20,6 +20,12 @@ class Solution {
         }
         return a;
     }
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int sum = getSum(a, b);
+        System.out.println("Sum of " + a + " and " + b + " is: " + sum);
+    }
 }
 
 
