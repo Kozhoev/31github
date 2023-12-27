71f4bf25a31aebc8286cb1b0871a60f6e66472b6
Kozhoev <kozhoevmamazait@gmail.com>
2023-12-27 10:10:35 +0600

    run

--- leedcode/problem35/Solution.java
+++ leedcode/problem35/Solution.java
@@ -12,4 +12,13 @@ class Solution {
         }
         return nums.length;
     }
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] nums = {1, 2, 3, 4, 5};
+        int target = 2;
+
+        int result = solution.searchInsert(nums, target);
+        System.out.println("Index where target would be inserted: " + result);
+    }
 }
